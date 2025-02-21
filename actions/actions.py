6355716835531659b5ec0ada6ed0b1e838f772e4
Heng Zhang <54906687+HengZ121@users.noreply.github.com>
2025-02-21 22:08:17 +0000

    rephraser added

--- actions/actions.py
+++ actions/actions.py
@@ -14,9 +14,9 @@ class ActionCheckSufficientFunds(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        # hard-coded balance for tutorial purposes. in production this
-        # would be retrieved from a database or an API
-        balance = 1000
-        transfer_amount = tracker.get_slot("amount")
-        has_sufficient_funds = transfer_amount <= balance
-        return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
+        # # hard-coded balance for tutorial purposes. in production this
+        # # would be retrieved from a database or an API
+        # balance = 1000
+        # transfer_amount = tracker.get_slot("amount")
+        # has_sufficient_funds = transfer_amount <= balance
+        # return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
